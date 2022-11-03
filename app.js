fbf57e069c83f5ea4a29f3b8e6c95952ccbaa8e8
johnfrancesalcano <johnfrancesalcano@gmail.com>
2022-11-03 17:35:46 +0800

    SECTION 11, JAVASCRIPT VIDEO 234

---
+++ app.js
@@ -0,0 +1,17 @@
+let age =26;
+let userName = 'Paran';
+let hobbies = ['gaming', 'Hangout', 'Eating'];
+let job = {
+title: 'Developer',
+place: 'New York',
+salary: 50000,
+};
+
+let adultYears = age - 12;
+alert(adultYears);
+
+age = 38;
+adultYears = age - 12;
+
+alert(adultYears);
+
